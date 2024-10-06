2a0faefcd7afcdae7ee276d94e39662e70315f9a
raghav <raghavgoel0212@gmail.com>
2024-10-06 22:18:45 +0530

    loader-addition

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,8 +1,12 @@
 export default function Loader() {
   return (
-    <div className="bg-black bg-opacity-50 flex items-center justify-center fixed left-0 right-0 bottom-0 top-0 z-50">
+    <div className="bg-black bg-opacity-75 flex items-center justify-center fixed left-0 right-0 bottom-0 top-0 z-50">
       <div>
-        <img src='/loader.svg' alt="Loading..." className="h-40" />
+        <img
+          src="\Ripple@1x-1.0s-200px-200px.svg"
+          alt="Loading..."
+          className="h-40"
+        />
       </div>
     </div>
   );
